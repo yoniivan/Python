261dac401925113e7f5741b3694eb68f2dc9f70b
yoniivan <42414267+yoniivan@users.noreply.github.com>
2019-12-15 06:33:16 +0200

    Update text_find_most_common.py

--- text_find_most_common.py
+++ text_find_most_common.py
@@ -1,11 +1,13 @@
 # Text / words file needs to be inside the same folder as the "words_text.txt" file.
 
-file = 'words_text_file'
+file = 'words_text_file.txt'
+
 
 def word_to_arr_from_file(file_path):
     arr = []
     with open(file_path, "r") as f:
         for line in f:
+            arr = [word for word in line.split()]
             for word in line.split():
                 arr.append(word)
     return arr
@@ -14,36 +16,38 @@ def word_to_arr_from_file(file_path):
 arr_words = word_to_arr_from_file(file)
 
 json_obj = {}
+
 json_obj["words"] = []
 
 arr_words_diff = []
 
+
 def unique():
     for i in arr_words:
         flag = False
-        length = len(json_obj["words"])
-        if length < 1:
+        if len(json_obj["words"]) < 1:
             word = {"word": i, "cnt": 1}
             json_obj["words"].append(word)
         else:
             for j in json_obj["words"]:
-                if j["word"] == i:
-                    j["cnt"] = j["cnt"] + 1
+                if j.get("word") == i:
+                    j.get("cnt") == j.get("cnt") + 1
                     flag = True
                     break
-            if flag == False:
+            if flag is not False:
                 word = {"word": i, "cnt": 1}
-                json_obj["words"].append(word)
+                json_obj.get("words").append(word)
 
 
 def find_max():
     unique()
-    max = 0
+
+    max_item = 0
     word = {}
     for i in json_obj["words"]:
-        if i["cnt"] > max:
-            max = i["cnt"]
-            word = i
+        if i.get("cnt") > max_item:
+            max_item, word = i["cnt"], i
     return word
+
 
 print(find_max())
