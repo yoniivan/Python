48496a679e0583f7e13844e01a60b48205e4e752
yoniivan <42414267+yoniivan@users.noreply.github.com>
2019-12-16 06:28:39 +0200

    Update text_find_most_common.py

--- text_find_most_common.py
+++ text_find_most_common.py
@@ -1,53 +1,29 @@
 # Text / words file needs to be inside the same folder as the "words_text.txt" file.
 
+from itertools import chain
+
 file = 'words_text_file.txt'
 
 
-def word_to_arr_from_file(file_path):
-    arr = []
-    with open(file_path, "r") as f:
+def file_to_generator(file_path):
+    with open(file_path) as f:
         for line in f:
-            arr = [word for word in line.split()]
-            for word in line.split():
-                arr.append(word)
-    return arr
+            yield line.strip('\n').split(' ')
 
 
-arr_words = word_to_arr_from_file(file)
-
-json_obj = {}
-
-json_obj["words"] = []
-
-arr_words_diff = []
-
-
-def unique():
-    for i in arr_words:
-        flag = False
-        if len(json_obj["words"]) < 1:
-            word = {"word": i, "cnt": 1}
-            json_obj["words"].append(word)
+def get_frequency(generator):
+    word_dict = dict()
+    for arr in chain.from_iterable(generator):
+        if len(word_dict) == 0:
+            word_dict.update({arr: 1})
         else:
-            for j in json_obj["words"]:
-                if j.get("word") == i:
-                    j.get("cnt") == j.get("cnt") + 1
-                    flag = True
-                    break
-            if flag is not False:
-                word = {"word": i, "cnt": 1}
-                json_obj.get("words").append(word)
+            if arr in word_dict:
+                word_dict[arr] += 1
+            else:
+                word_dict.update({arr: 1})
+    max_item = max(word_dict, key=lambda k: word_dict[k])
+    max_quantity = word_dict.get(max_item)
+    return max_item, max_quantity
 
 
-def find_max():
-    unique()
-
-    max_item = 0
-    word = {}
-    for i in json_obj["words"]:
-        if i.get("cnt") > max_item:
-            max_item, word = i["cnt"], i
-    return word
-
-
-print(find_max())
+print(get_frequency(file_to_generator(file)))
