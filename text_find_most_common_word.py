2acb1f64d00f0dcaed336681ec861b1a1003efbf
yoniivan <42414267+yoniivan@users.noreply.github.com>
2019-12-26 05:59:56 +0200

    Update text_find_most_common_word.py

--- text_find_most_common_word.py
+++ text_find_most_common_word.py
@@ -1,5 +1,4 @@
 # Gets as a param a text file woth words and returns the most frequency word as a Tuple(key, value)
-
 from itertools import chain
 
 file = 'words_text_file.txt'
@@ -17,10 +16,7 @@ def get_frequency(generator):
         if len(word_dict) == 0:
             word_dict.update({arr: 1})
         else:
-            if arr in word_dict:
-                word_dict[arr] += 1
-            else:
-                word_dict.update({arr: 1})
+            word_dict[arr] = word_dict.get(arr, 0) + 1
     max_item = max(word_dict, key=lambda k: word_dict[k])
     max_quantity = word_dict.get(max_item)
     return max_item, max_quantity
